8f6c4d19a6c95bdc8af080af23957d44fed0f972
Peter <pbreid@gmail.com>
2024-06-21 11:39:03 +1000

    added progress bar

--- src/livox_to_pointcloud2.py
+++ src/livox_to_pointcloud2.py
@@ -8,7 +8,7 @@ from sensor_msgs.msg import PointCloud2, PointField
 from sensor_msgs import point_cloud2
 from std_msgs.msg import Header
 from custom_msgs.msg import CustomMsg  # Replace with the correct import for CustomMsg
-
+from tqdm import tqdm
 
 def custom_to_pointcloud2(custom_msg, frame_id, timestamp):
     header = Header()
@@ -32,21 +32,24 @@ def custom_to_pointcloud2(custom_msg, frame_id, timestamp):
     pointcloud2_msg.is_dense = True  # Set is_dense to True if all points are valid
     return pointcloud2_msg
 
-
 def signal_handler(sig, frame):
     rospy.loginfo('Interrupted! Exiting...')
     sys.exit(0)
 
 def process_bag(input_bag, output_bag, topics):
-    with rosbag.Bag(output_bag, 'w') as outbag:
-        rospy.loginfo("Started processing the bag file.")
+    rospy.loginfo("Counting messages in the bag file...")
+    
+    # Get the total number of messages quickly using get_message_count
+    with rosbag.Bag(input_bag, 'r') as bag:
+        total_messages = bag.get_message_count()
+
+    rospy.loginfo(f"Total messages to process: {total_messages}")
+
+    with rosbag.Bag(output_bag, 'w') as outbag, tqdm(total=total_messages, desc="Processing messages", unit="msg") as pbar:
         for topic, msg, t in rosbag.Bag(input_bag).read_messages():
             # Only write the original message if it is not in the output topics list
             if topic not in topics.values():
                 outbag.write(topic, msg, t)
-
-            #if topic == '/dead_reckoning/pose':
-            #    rospy.loginfo(f"Processed original message on topic: {topic}")
 
             # Check if the message is from one of the specified input topics
             if topic in topics:
@@ -54,13 +57,13 @@ def process_bag(input_bag, output_bag, topics):
                 output_topic = topics[topic]
                 outbag.write(output_topic, pointcloud2_msg, t)
 
-        rospy.loginfo("Finished processing the bag file.")
+            pbar.update(1)
 
 if __name__ == '__main__':
     rospy.init_node('process_bag_node')
 
     signal.signal(signal.SIGINT, signal_handler)
-    
+
     input_bag = rospy.get_param('~input_bag')
     output_bag = rospy.get_param('~output_bag')
     
